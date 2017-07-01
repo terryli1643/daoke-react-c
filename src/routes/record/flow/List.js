59590824469e82eeddca7a9fed63fb84e6458df7
lizhen <lizhen@idealsee.cn>
2017-07-01 22:45:34 +0800

    修改时间线为最后一个红色

--- src/routes/record/flow/List.js
+++ src/routes/record/flow/List.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Timeline } from 'antd'
+import { Timeline, Icon } from 'antd'
 
 const List = ({ flowData }) => {
   const list = []
-  flowData.forEach((item) => {
-    list.push(<Timeline.Item>{item.time} | {item.data}</Timeline.Item>)
+  flowData.forEach((item, index) => {
+    if (index === 0) {
+      list.push(<Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red" style={{ color: 'red' }}>{item.time} | {item.data}</Timeline.Item>)
+    } else {
+      list.push(<Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>{item.time} | {item.data}</Timeline.Item>)
+    }
   })
   return (
     <div>
